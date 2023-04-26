42ab9502e9f57d51c2f8b7807815a46a28f71d03
Alyssa Chen <alyssachen@college.harvard.edu>
2023-04-26 16:21:15 -0400

    cleaned up endgame, allow multiple guesses in one turn, and combined new_word procedures

--- anagram_lib.py
+++ anagram_lib.py
@@ -100,6 +100,7 @@ class Board():
 		self.words = words # dictionary with players mapping to words owned by that player
 		self.letters_down = letters_from_dist(letters_dist) # list of letters not yet visible
 		self.letters_up = [] # list of visible, upturned letters
+		self.end_game = False # indicator to end the game when no more letters_down AND no new anagrams created
 
 	def __str__(self):
 		s = ""
@@ -115,17 +116,22 @@ class Board():
 			s+= f"\nPLAYER: {player}"
 			for word in self.words[player]:
 				s+= f"\n   {word}"
+		s+= "\n"
 		return s
 
-	def flip_letter(self):
+	def flip_letter(self, n = 1):
 		"""
-		Turns over a letter if there are letters left. If not, game ends. 
+		Turns over n letters if there are letters left. 
+		If there are <n letters left, turn over the remaining letters
+		and begin last turn. 
 		"""
-		try:
-			self.letters_up.append(self.letters_down.pop())
-		except:
-			print("No more letters!")
-			self.end_game()
+
+		for i in range(n):
+			try:
+				self.letters_up.append(self.letters_down.pop())
+			except:
+				print("No more letters! Last chance to create ANAGRAMS!")
+				self.end_game = True # this doesn't immediately end the game but rather triggers a game ending the next turn
 
 	def take_turn(self):
 		"""
@@ -137,9 +143,9 @@ class Board():
 		# now it's the next player's turn
 		self.whos_turn = (self.whos_turn + 1) % len(self.players)
 
-	def end_game(self):
+	def score(self):
 		"""
-		Ends game. Calculates scores and prints out winner. 
+		Calculates scores and prints out winner. 
 		Score for each player is calculated by adding the (length-3) of each word
 		in order to incentivize longer words. 
 		"""
@@ -158,45 +164,21 @@ class Board():
 		for w in winners:
 			print(f"{self.players[w]} won! ")
 
+
 		print("Game ended.")
 
-	def word_from_letters(self, new_player, new_word):
+	def new_word(self, new_player, new_word):
 		"""
-		Player enters a new word from the upturned letters. Check to see if
+		Player enters a new word. Check to see if
 		it is a valid new word, i.e. the following conditions hold:
+
+		OPTION A: the new word is created only from the center letters
 		1. New word is in the Scrabble dictionary.
 		2. New word is an anagram of some subset of the upturned letters. 
-		"""
 
-		# check that new word is in Scrabble dictionary
-		check_0 = is_word(new_word)
+		OR
 
-		zeros = np.array([0 for x in range(26)]) # array of 26 zeros for comparison
-
-		# check that new word is a subset of available letters 
-		# (i.e. new word can only add available letters to old word)
-		check_2 = np.all(letter_freqs(self.letters_up) - letter_freqs(new_word) >= zeros)
-
-		if not (check_0 & check_2): 
-			print("This is not a valid new word!")
-			return
-
-		# If it is a valid word, remove old word from old player and used letters from
-		# upturned letters; add new word to new player. 
-		self.letters_up = freqs_to_letters(letter_freqs(self.letters_up) - letter_freqs(new_word))
-		self.words[new_player].append(new_word)
-
-		# Flip over two new letters if a new word is successfully created
-		self.flip_letter()
-		self.flip_letter()
-
-		print(f"{new_player} has created the word {new_word}!")
-
-
-	def word_from_word(self, new_player, old_word, new_word): 
-		"""
-		Player enters a new word out of an old word. Check to see if 
-		it is a valid new word, i.e. the following conditions hold:
+		OPTION B: the new word is created from the center letters and an old word
 		1. New word is in the Scrabble dictionary. 
 		2. New word is an anagram of the old word + some subset of upturned letters,
 		i.e. new word is longer than old word,
@@ -204,39 +186,65 @@ class Board():
 		and only adds available upturned letters to the old word. 
 		"""
 
-		# checking if old word exists and who the old word belongs to
-		old_player = None
-		for player in self.words:
-			for word in self.words[player]:
-				if word == old_word: 
-					old_player = player
-
-		if not old_player:
-			print("The word you are trying to steal does not exist!")
-			return
-
+		# check that new word is in Scrabble dictionary
+		if not is_word(new_word): 
+			print("Not a valid new word. Try again!")
+			return False
 
 		zeros = np.array([0 for x in range(26)]) # array of 26 zeros for comparison
 
-		# total set of available letters (old word letters AND upturned letters)
-		available_letters = letter_freqs(old_word) + letter_freqs(self.letters_up)
+		# OPTION A: check if new word can be made of the upturned letter tiles
+		check_1 = np.all(letter_freqs(self.letters_up) - letter_freqs(new_word) >= zeros)
 
-		# check that new word is in Scrabble dictionary
-		check_0 = is_word(new_word)
+		if check_1: 
+			# If it is a valid word from the upturned tiles, 
+			# remove old word from old player and used letters from
+			# upturned letters; add new word to new player. 
+			self.letters_up = freqs_to_letters(letter_freqs(self.letters_up) - letter_freqs(new_word))
+			self.words[new_player].append(new_word)
 
-		# check that old word is a subset of new word (i.e. new word needs to use up entire old word)
-		check_1 = np.all(letter_freqs(new_word)-letter_freqs(old_word) >= zeros)
+			# Flip over two new letters if a new word is successfully created
+			self.flip_letter(2)
 
-		# check that new word is a subset of old word + available letters 
-		# (i.e. new word can only add available letters to old word)
-		check_2 = np.all(available_letters - letter_freqs(new_word) >= zeros)
+			print(f"{new_player} has created the word {new_word}!")
+			return True
 
-		# check that new word is longer than old word
-		check_3 = len(new_word) > len(old_word)
 
-		if not (check_0 & check_1 & check_2 & check_3): 
-			print("This is not a valid new word!")
-			return
+		# OPTION B: checking if old word exists and who the old word belongs to
+		old_player = None
+		old_word = None
+		to_break = False
+
+		for player in self.words:
+			for word in self.words[player]:
+
+				# check to see if the new word can be made out of this word
+				available_letters = letter_freqs(word) + letter_freqs(self.letters_up)
+
+				# check that old word is a subset of new word (i.e. new word needs to use up entire old word)
+				check_1 = np.all(letter_freqs(new_word)-letter_freqs(word) >= zeros)
+
+				# check that new word is a subset of old word + available letters 
+				# (i.e. new word can only add available letters to old word)
+				check_2 = np.all(available_letters - letter_freqs(new_word) >= zeros)
+
+				# check that new word is longer than old word
+				check_3 = len(new_word) > len(word)
+
+				# found a valid old word! 
+				if check_1 & check_2 & check_3: 
+					old_player = player
+					old_word = word
+					to_break = True
+
+				if to_break: break
+
+			if to_break: break
+
+		if not old_player:
+			print("Not a valid steal!")
+			return False
+
 
 		# If it is a valid word, remove old word from old player and used letters from
 		# upturned letters; add new word to new player. 
@@ -246,10 +254,17 @@ class Board():
 		self.words[new_player].append(new_word)
 
 		# Flip over two new letters if a new word is successfully created
-		self.flip_letter()
-		self.flip_letter()
+		self.flip_letter(2)
 
-		print(f"{new_player} has created the word {new_word} out of {old_word}!")
+		if new_player == old_player:
+			print(f"{new_player} has created the word {new_word} out of {old_word}!")
+		else:
+			print(f"{new_player} has stolen {old_word} from {old_player} to create {new_word}!")
+		return True
+
+
+
+
 
 
 
--- non_networked.py
+++ non_networked.py
@@ -9,32 +9,43 @@ from anagram_lib import Board
 
 def main():
     print("ANAGRAMS!")
-    players = input("Enter player names separated by spaces: ")
+
+    while True: 
+        players = input("Enter player names separated by spaces: ")
+        if not len(players) == 0:
+            break
+        print("Please enter at least one player name. ")
 
     # initialize board
-    board = Board(players.split())
+    players = players.split()
+    board = Board(players)
 
     # gameplay
-    while len(board.letters_down) + len(board.letters_up) > 0:
-        board.take_turn()
+    while len(board.letters_down) + len(board.letters_up) > 0 and not board.end_game:
 
+        board.take_turn()
         print(board)
 
-        new_word = input("Input an ANAGRAM if you can find one! Otherwise, press enter: ").upper()
-        if new_word == "":
-            continue
+        while True: 
+            new_word = input("Input an ANAGRAM if you can find one! Otherwise, press enter: ").upper()
+            if new_word == "":
+                break
 
-        old_word = input("If this is an ANAGRAM of the letters, press enter. Otherwise, enter the word you are changing: ").upper()
-        
-        while True:
-            new_player = input("Enter name of player who is making this word: ")
-            if new_player in players: break
-            print("This player name doesn't exist! Try again. ")
+            # code to end game
+            if new_word == "=":
+                board.end_game = True
+                break
 
-        if old_word == "":
-            board.word_from_letters(new_player, new_word)
-        else:
-            board.word_from_word(new_player, old_word, new_word)
+            while True:
+                new_player = input("Enter name of player who is making this word: ")
+                if new_player in players: break
+                print("This player name doesn't exist! Try again. ")
+
+            if board.new_word(new_player, new_word):
+                print(board)
+
+    board.score()
+
 
 
 
